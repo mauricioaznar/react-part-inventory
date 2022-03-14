01cafad3f2def8196e4223b560127ca82b7006e7
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-14 12:55:12 -0600

    raw materials commit

--- src/constants/api-url.ts
+++ src/constants/api-url.ts
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === "development"
     ? "localhost:3007"
-    : "sandbox-server.mauaznar.com";
+    : "subnautica-server.mauaznar.com";
 
 export default apiUrl;
