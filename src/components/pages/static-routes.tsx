84cde6855cec64ef5bd9d0753a7a0ce59929348b
mauricioaznar <mauricioaznar94@gmail.com>
2022-03-22 13:33:39 -0600

    refactoring

--- src/components/pages/static-routes.tsx
+++ src/components/pages/static-routes.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import SchemaIcon from "@mui/icons-material/Schema";
-import ScienceIcon from "@mui/icons-material/Science";
 import HomePage from "./home-page/home-page";
 import { Route, RouteGroup } from "../../types/route";
 import ErDiagramPage from "./technical/er-diagram-page";
-import TestingPage from "./home-page/testing-page";
 
 export const staticRouteGroups: RouteGroup[] = [
     {
@@ -16,15 +14,6 @@ export const staticRouteGroups: RouteGroup[] = [
                 path: "/",
                 component: <HomePage />,
                 title: "Home",
-                navbar: true,
-                exact: true,
-            },
-            {
-                icon: <ScienceIcon />,
-                name: "TestingPage",
-                path: "/testing",
-                component: <TestingPage />,
-                title: "Testing",
                 navbar: true,
                 exact: true,
             },
